741171fbb9b1a2c6e0aaba2047c1abbd2a9c265d
David Jones <djones6@uk.ibm.com>
2017-06-13 13:58:03 +0100

    Support describing Swift 3.0.x builds

--- lib/build.sh
+++ lib/build.sh
@@ -55,50 +55,71 @@ function build {
 # If a repository is specified, then NEW_COMMIT will be set to the short commit hash.
 #
 function describeBuild {
-  local pathname=$1	# The build name (build directory)
-  local repo=$2		# The repository under evaluation
-  local commit=$3 	# The commit that we want to evaluate
-  local branchname=$4	# The branch name for the commit (for display purposes only)
+  local name=$1		# The build name (for display purposes only)
+  local path=$2		# The build directory (containing .build)
+  local repo=$3		# The repository under evaluation
+  local commit=$4 	# The commit that we want to evaluate
+  local branchname=$5	# The branch name for the commit (for display purposes only)
 
-  local name=$(basename $pathname)
+  pushd $path
+  local swiftVersion=`cat .swift-version`
 
   json_object_start "$name"
-  json_string "Swift" "`cat .swift-version`"
+  json_string "Swift" "$swiftVersion"
 
-  # Note: this relies on Swift 3.1 build layout (SPM implementation detail)
-  for package in $pathname/checkouts/*
-  do
-    cd $package
-    package=`basename $package`
-    package="${package%.git*}"
+  case "$swiftVersion" in
+  3.0|3.0.1|3.0.2)
+    for package in Packages/*
+    do
+      cd $package
+      package=`basename $package`
+      branch=`git symbolic-ref --short HEAD`
 
-    json_object_start "$package"
+      json_object_start "${package%-*}"
+      json_string "branch" "${branch##*heads/}"
+      json_string "commit" "`git rev-parse HEAD | xargs git rev-parse --short`"
+      json_string "message" "`git show -s --format=%B $commit`"
+      json_object_end
+      cd -
+    done
+    ;;
+  *)
+    # Note: this relies on Swift 3.1 build layout (SPM implementation detail)
+    for package in .build/checkouts/*
+    do
+      cd $package
+      package=`basename $package`
+      package="${package%.git*}"
 
-    if [ "$package" = "$repo" ]; then
-      local hash=`git rev-parse --short $commit`
-      # We should be able to get the branch name here. Tail because the first
-      # item listed will be our detached HEAD state.
-      local branch=`git branch --contains $commit | tail -n 1`
-      if [ ! "$branchname" = "$branch" ]; then
-          echo "Warning, provided branch name $branchname does not match inferred branch name $branch, using $branchname anyway"
-          branch=$branchname
+      json_object_start "$package"
+
+      if [ "$package" = "$repo" ]; then
+        local hash=`git rev-parse --short $commit`
+        # We should be able to get the branch name here. Tail because the first
+        # item listed will be our detached HEAD state.
+        local branch=`git branch --contains $commit | tail -n 1`
+        if [ ! "$branchname" = "$branch" ]; then
+            echo "Warning, provided branch name $branchname does not match inferred branch name $branch, using $branchname anyway"
+            branch=$branchname
+        fi
+        # Overwrite NEW_COMMIT with short hash for later
+        NEW_COMMIT=$hash
+      else
+        local hash=`git rev-parse HEAD | xargs git rev-parse --short`
+        local branch=`git describe --all`
+        branch="${branch##*tags/}"
       fi
-      # Overwrite NEW_COMMIT with short hash for later
-      NEW_COMMIT=$hash
-    else
-      local hash=`git rev-parse HEAD | xargs git rev-parse --short`
-      local branch=`git describe --all`
-      branch="${branch##*tags/}"
-    fi
 
-    json_string "branch" "$branch"
-    json_string "commit" "$hash"
-    json_string "message" "`git show -s --format=%B $hash`"
-    json_object_end
-    cd -
-  done
-
+      json_string "branch" "$branch"
+      json_string "commit" "$hash"
+      json_string "message" "`git show -s --format=%B $hash`"
+      json_object_end
+      cd -
+    done
+    ;;
+  esac
   json_object_end
+  popd
 }
 
 #
